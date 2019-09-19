9f367258021b5221a4abafe7e9a858f0bf37476c
Vinicius Cambraia <53883408+ViniciusCambraia@users.noreply.github.com>
2019-09-18 23:40:29 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
-public class Main {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		Extrator E = new Extrator("https://www.urionlinejudge.com.br/judge/pt/profile/372143");
-		System.out.println(E);
-		    }
-		}
+public class Main {
+	static Scanner entrada = new Scanner(System.in);
+		public static void main(String[] args) {
+			int a= entrada.nextInt();
+			
+			Extrator E = new Extrator("https://www.urionlinejudge.com.br/judge/pt/profile/"+a);
+			System.out.println(E);
+			    }
+			
